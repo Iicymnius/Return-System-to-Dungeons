6faf0b4f0acd0c52bdf8fee0bd80e91943bcc26f
Licymnius <licymnius@outlook.com>
2021-10-17 09:37:40 +0300

    Update char.h

--- SVN/Server/game/src/char.h
+++ SVN/Server/game/src/char.h
@@ -8,10 +8,12 @@
 
 //find again;
 
+};
+
 ESex GET_SEX(LPCHARACTER ch);
 
 //add above;
 
 public:
-	long	LicyPlayerDungeonIndex()	{ return m_points.d_map_index; }
-	void	MniusPlayerDungeonIndex(long vnum)	{ m_points.d_map_index = vnum; }
+	long	LicyPlayerDungeonIndex() { return m_points.d_map_index; }
+	void	MniusPlayerDungeonIndex(long vnum) { m_points.d_map_index = vnum; }
